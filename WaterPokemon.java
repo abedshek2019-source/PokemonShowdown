f4c5d7d546c95d1b227737cb81dbbceaf8cecd6c
abedshek2019-source <abed.shek2019@gmail.com>
2026-09-10 13:48:19 -0600

    Update WaterPokemon.java

--- WaterPokemon.java
+++ WaterPokemon.java
@@ -1,1 +1,10 @@
+public class WaterPokemon extends Pokemon {
+    public WaterPokemon(String name, int maxHp, Move[] moves) {
+        super(name, "Water", maxHp, moves);
+    }
 
+    @Override
+    public void performSpecialAction(Pokemon target) {
+        System.out.println(name + " splashes water around, creating a humid mist!");
+    }
+}
