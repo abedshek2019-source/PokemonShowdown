3ec3e09e0711e0d13c063aad2a5472aedf42cfc9
abedshek2019-source <abed.shek2019@gmail.com>
2026-09-10 13:40:09 -0600

    Create Move.java

---
+++ Move.java
@@ -0,0 +1,11 @@
+public class Move {
+    String name;
+    String type;
+    int power;
+
+    public Move(String name, String type, int power) {
+        this.name = name;
+        this.type = type;
+        this.power = power;
+    }
+}
